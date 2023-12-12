303c3065ce4283fb0c4c230bf9057bea45a701b0
Linkdom <donglinkang2021@163.com>
2023-12-12 12:15:13 +0800

    add WinMain

--- masmGameApp/main.asm
+++ masmGameApp/main.asm
@@ -1,6 +1,7 @@
 .386
 .model flat, stdcall
 option casemap:none
+
 include windows.inc
 include kernel32.inc
 includelib kernel32.lib
@@ -8,11 +9,23 @@ include user32.inc
 includelib user32.lib
 
 .data
-MsgCaption      db "Message Box",0
-MsgBoxText      db "This is a Message",0
+	MsgCaption      db "Message Box",0
+	MsgBoxText      db "This is a Message",0
 
 .code
-main:
-	invoke MessageBox, NULL,addr MsgBoxText, addr MsgCaption, MB_OK
-	invoke ExitProcess,NULL
-end main
+	;------------------------------------------
+	; WinMain - Entry point for our program
+	; @param 
+	; @author linkdom
+	;------------------------------------------
+	WinMain PROC
+		invoke MessageBox, NULL,addr MsgBoxText, addr MsgCaption, MB_OK
+	WinMain ENDP
+
+
+	main PROC
+		call WinMain
+		invoke ExitProcess,NULL
+		ret ; return to operating system, else stack will be corrupted
+	main ENDP
+	end main
