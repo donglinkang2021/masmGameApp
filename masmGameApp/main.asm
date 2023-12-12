e1526102088d1e7187ab90676df5e830bd06872f
Linkdom <donglinkang2021@163.com>
2023-12-12 22:41:00 +0800

    显示多张图片

--- masmGameApp/main.asm
+++ masmGameApp/main.asm
@@ -10,21 +10,160 @@ includelib user32.lib
 include gdi32.inc
 includelib gdi32.lib
 
+
+
 .data
-	MyWinClass   db "Simple Win Class",0
-	AppName      db "My First Window",0
 	stRect RECT <0,0,0,0>;客户窗口的大小，right代表长，bottom代表高
+	
 
 .data?
 	hInstance dword ? 	;程序的句柄
 	hWinMain dword ?	;窗体的句柄
-	hBitmap dd ?		;bitmap图片的句柄
+	
+	; bitmap
+	hBmpTest dd ?		;测试图片的句柄
+	hBmpBack dd ?		;背景图片的句柄
+
+	ITEMBMP struct
+		hbp dd ? 	;位图的句柄
+		x dd ? 		;位图x坐标
+		y dd ?		;位图y坐标
+		w dd ?		;位图宽度
+		h dd ?		;位图高度
+		flag dd ?	;位图的展示方式
+	ITEMBMP ends
+	items ITEMBMP 4096 dup(<?,?,?,?,?,?>)
+	itemsCount dd 0	;当前已经加载的图片的数量
 
 .const
+	MyWinClass   db "Simple Win Class",0
+	AppName      db "My First Window",0
 	IDI_ICON1 equ 102 	;图标的ID
-	IDB_BITMAP1 equ 103 ; 希望加载的一张bitmap图片的ID
+	IDB_TEST equ 103 	;测试图片的ID
+	IDB_BACK equ 104 	;背景图片的ID
 
 .code
+
+	;------------------------------------------
+	; LoadAllBmp - 加载所有的图片
+	; @param
+	; @return void
+	;------------------------------------------
+	LoadAllBmp PROC
+		invoke LoadBitmap, hInstance, IDB_TEST
+		mov hBmpTest, eax
+		invoke LoadBitmap, hInstance, IDB_BACK
+		mov hBmpBack, eax
+		ret
+	LoadAllBmp ENDP
+
+
+	;------------------------------------------
+	; DeleteBmp - 删除所有的图片
+	; @param
+	; @return void
+	;------------------------------------------
+	DeleteBmp PROC
+		invoke DeleteObject, hBmpTest
+		invoke DeleteObject, hBmpBack
+		ret
+	DeleteBmp ENDP
+
+
+	;------------------------------------------
+	; Bmp2Buffer - 将图片绘制到缓冲区
+	; @param hBmp:HBITMAP
+	; @param x:DWORD
+	; @param y:DWORD
+	; @param w:DWORD
+	; @param h:DWORD
+	; @param flag:DWORD
+	; @return void
+	;------------------------------------------
+	Bmp2Buffer PROC hBmp:DWORD, x:DWORD, y:DWORD, w:DWORD, h:DWORD, flag:DWORD
+		; get the top buffer
+		mov eax, itemsCount
+		mov edi, offset items
+		mov ebx, TYPE ITEMBMP
+		mul ebx
+		add edi, eax
+
+		; set the buffer
+		mov eax, hBmp
+		mov (ITEMBMP PTR [edi]).hbp, eax
+		mov eax, x
+		mov (ITEMBMP PTR [edi]).x, eax
+		mov eax, y
+		mov (ITEMBMP PTR [edi]).y, eax
+		mov eax, w
+		mov (ITEMBMP PTR [edi]).w, eax
+		mov eax, h
+		mov (ITEMBMP PTR [edi]).h, eax
+		mov eax, flag
+		mov (ITEMBMP PTR [edi]).flag, eax
+
+		; add the count
+		inc itemsCount
+		ret
+	Bmp2Buffer ENDP
+
+
+	;------------------------------------------
+	; Buffer2Window - 将缓冲区的图片绘制到窗口
+	; @param hWnd:HWND
+	; @return void
+	;------------------------------------------
+	Buffer2Window PROC
+		LOCAL ps:PAINTSTRUCT
+		LOCAL hdc:dword ;屏幕的hdc 全称是handle device context
+		LOCAL hdc1:dword;缓冲区1
+		LOCAL hdc2:dword;缓冲区2
+		LOCAL hBmp:dword;缓冲区的位图
+		LOCAL @bminfo :BITMAP
+
+		invoke BeginPaint, hWinMain, addr ps
+		mov hdc, eax
+		invoke CreateCompatibleDC, hdc
+		mov hdc1, eax
+		invoke CreateCompatibleDC, hdc
+		mov hdc2, eax
+
+		; get the window size
+		invoke CreateCompatibleBitmap,hdc,stRect.right,stRect.bottom
+		mov hBmp,eax
+		invoke SelectObject,hdc1,hBmp
+		invoke SetStretchBltMode,hdc,HALFTONE
+		invoke SetStretchBltMode,hdc1,HALFTONE
+
+		mov esi, 0
+		mov edi, offset items
+		.while esi < itemsCount
+			invoke GetObject,(ITEMBMP PTR [edi]).hbp,type @bminfo,addr @bminfo
+			invoke SelectObject,hdc2,(ITEMBMP PTR [edi]).hbp
+			invoke StretchBlt,hdc1,\
+				(ITEMBMP PTR [edi]).x,(ITEMBMP PTR [edi]).y,\
+				(ITEMBMP PTR [edi]).w,(ITEMBMP PTR [edi]).h,\
+				hdc2,0,0,\
+				@bminfo.bmWidth,@bminfo.bmHeight,\
+				(ITEMBMP PTR [edi]).flag
+			inc esi
+			add edi,TYPE ITEMBMP
+		.endw
+
+		invoke StretchBlt,hdc,0,0,\
+			stRect.right,stRect.bottom,\
+			hdc1,0,0,\
+			stRect.right,stRect.bottom,\
+			SRCCOPY
+
+		invoke DeleteDC,hBmp
+		invoke DeleteDC,hdc2
+		invoke DeleteDC,hdc1
+		invoke DeleteDC,hdc
+		invoke EndPaint, hWinMain, addr ps
+		mov itemsCount, 0
+		ret
+	Buffer2Window ENDP
 
 	;------------------------------------------
 	; WndProc - Window procedure
@@ -36,27 +175,18 @@ includelib gdi32.lib
 	; @author linkdom
 	;------------------------------------------
 	WndProc PROC hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
-		LOCAL ps:PAINTSTRUCT
-		LOCAL hdc:HDC
-		LOCAL hMemDC:HDC
 		.if uMsg==WM_DESTROY
-			invoke DeleteObject, hBitmap
-			invoke	DestroyWindow,hWinMain
+			invoke DeleteBmp
+			invoke DestroyWindow, hWinMain
 			invoke PostQuitMessage, NULL
 		.elseif uMsg==WM_CREATE
-			; invoke MessageBox, NULL, addr AppName, addr MyWinClass, MB_OK
-			invoke LoadBitmap, hInstance, IDB_BITMAP1
-			mov hBitmap, eax
-		.elseif uMsg == WM_PAINT
-			invoke BeginPaint, hWnd, addr ps
-			mov hdc, eax
+			invoke LoadAllBmp
 			invoke GetClientRect, hWnd, addr stRect
-			invoke CreateCompatibleDC, hdc
-			mov hMemDC, eax
-			invoke SelectObject, hMemDC, hBitmap
-			invoke BitBlt, hdc, 0, 0, stRect.right, stRect.bottom, hMemDC, 0, 0, SRCCOPY
-			invoke DeleteDC, hMemDC
-			invoke EndPaint, hWnd, addr ps
+			; 这里之后实现动态波浪移动
+		.elseif uMsg == WM_PAINT
+			invoke Bmp2Buffer, hBmpBack, 0, 0, stRect.right, stRect.bottom, SRCCOPY
+			invoke Bmp2Buffer, hBmpTest, 376, 276, 48, 48, SRCCOPY
+			invoke Buffer2Window
 		.else
 			invoke DefWindowProc, hWnd, uMsg, wParam, lParam		
 			ret
@@ -96,7 +226,7 @@ includelib gdi32.lib
 				100,100,800,600,\	;窗口的位置和大小
 				NULL,NULL,hInstance,NULL
 		mov	hWinMain,eax
-		invoke ShowWindow, hWinMain,SW_SHOWDEFAULT 
+		invoke ShowWindow, hWinMain, SW_SHOWDEFAULT 
 		invoke UpdateWindow, hWinMain 
 		.while	TRUE
 			invoke	GetMessage,addr @stMsg,NULL,0,0
--- masmGameApp/resource.h
+++ masmGameApp/resource.h
@@ -1,15 +1,16 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ Éú³ÉµÄ°üº¬ÎÄ¼þ¡£
-// ¹© Resource.rc Ê¹ÓÃ
+// Microsoft Visual C++ ï¿½ï¿½ï¿½ÉµÄ°ï¿½ï¿½ï¿½ï¿½Ä¼ï¿½ï¿½ï¿½
+// ï¿½ï¿½ Resource.rc Ê¹ï¿½ï¿½
 //
 #define IDI_ICON1                       102
-#define IDB_BITMAP1                     103
+#define IDB_TEST                        103
+#define IDB_BACK                        104
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        109
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
