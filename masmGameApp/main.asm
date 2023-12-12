9bdfa98252210dbae920f58a63552eb2c8ff1455
Linkdom <donglinkang2021@163.com>
2023-12-12 13:13:03 +0800

    my first window app

--- masmGameApp/main.asm
+++ masmGameApp/main.asm
@@ -9,17 +9,74 @@ include user32.inc
 includelib user32.lib
 
 .data
-	MsgCaption      db "Message Box",0
-	MsgBoxText      db "This is a Message",0
+	MyWinClass   db "Simple Win Class",0
+	AppName      db "My First Window",0
+
+.data?
+	hInstance dword ? 	;程序的句柄
+	hWinMain dword ?	;窗体的句柄
 
 .code
+
+	;------------------------------------------
+	; WndProc - Window procedure
+	; @param hWnd:HWND
+	; @param uMsg:UINT
+	; @param wParam:WPARAM
+	; @param lParam:LPARAM
+	; @return LRESULT
+	; @author linkdom
+	;------------------------------------------
+	WndProc PROC hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
+		.if uMsg==WM_DESTROY
+			invoke PostQuitMessage, NULL
+		.else
+			invoke DefWindowProc, hWnd, uMsg, wParam, lParam		
+			ret
+		.endif
+		xor eax,eax
+		ret
+	WndProc ENDP
+
 	;------------------------------------------
 	; WinMain - Entry point for our program
 	; @param 
 	; @author linkdom
 	;------------------------------------------
 	WinMain PROC
-		invoke MessageBox, NULL,addr MsgBoxText, addr MsgCaption, MB_OK
+		local	@stWndClass:WNDCLASSEX
+		local	@stMsg:MSG
+		invoke	GetModuleHandle,NULL
+		mov	hInstance,eax	;获取程序的句柄
+		invoke	RtlZeroMemory,addr @stWndClass,sizeof @stWndClass
+		invoke	LoadCursor,0,IDC_ARROW
+		mov	@stWndClass.hCursor,eax
+		invoke LoadIcon, hInstance, 102; 
+		mov	@stWndClass.hIcon,eax
+		mov	@stWndClass.hIconSm,eax
+		push hInstance
+		pop	@stWndClass.hInstance
+		mov	@stWndClass.cbSize,sizeof WNDCLASSEX
+		mov	@stWndClass.style,CS_HREDRAW or CS_VREDRAW
+		mov	@stWndClass.lpfnWndProc,offset WndProc ;指定窗口处理程序
+		mov	@stWndClass.hbrBackground,COLOR_WINDOW + 1
+		mov	@stWndClass.lpszClassName,offset MyWinClass;窗口的类名
+		invoke	RegisterClassEx,addr @stWndClass
+		invoke	CreateWindowEx,WS_EX_CLIENTEDGE,\
+				offset MyWinClass,\
+				offset AppName,\
+				WS_OVERLAPPEDWINDOW,\
+				100,100,800,600,\
+				NULL,NULL,hInstance,NULL
+		mov	hWinMain,eax
+		invoke ShowWindow, hWinMain,SW_SHOWDEFAULT 
+		invoke UpdateWindow, hWinMain 
+		.while	TRUE
+			invoke	GetMessage,addr @stMsg,NULL,0,0
+			.break	.if eax	== 0
+			invoke	TranslateMessage,addr @stMsg
+			invoke	DispatchMessage,addr @stMsg
+		.endw
 		ret
 	WinMain ENDP
 
